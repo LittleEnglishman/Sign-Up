58310530450d3fd0cfb4cdffbfb8c309ae0d2513
LittleEnglishman <nztomking@gmail.com>
2019-02-13 15:00:36 +1300

    Python ticket dictionary made

---
+++ signup.py.py
@@ -0,0 +1,7 @@
+from bottle import run, route, get, push
+tickets = [
+    Ticket("Thomas John King", "tomking@email.exmail","19/07/2001"),
+    Ticket("Moses Wescombe", "moseswescombe@email.email", "16/11/2007"),
+    Ticket("Jeremy Roberts", "jerryisdope.com", "20/02/2009"),
+    Ticket('Ariana Grande', "iwishihasheremail.gamil.com", "14/08/1998")
+    ]
