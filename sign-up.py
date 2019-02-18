a08b9205b4fd8752ba51cd496d49c88ee97ad078
LittleEnglishman <nztomking@gmail.com>
2019-02-18 20:46:47 +1300

    Add files via upload
    Adde din images, as well as fixed code adn cleaned up

--- sign-up.py
+++ sign-up.py
@@ -1,4 +1,4 @@
-from bottle import run, route, get, post, request, view
+from bottle import run, route, get, post, request, view, static_file
 from itertools import count
 #Build Log
 #V1.1 Creation of file plus test data
@@ -21,6 +21,11 @@ tickets = [
     Ticket("Jeremy Roberts", "jerryisdope.com", "20/02/2009", True),
     Ticket('Ariana Grande', "iwishihasheremail.gamil.com", "14/08/1998", False)
     ]
+#Images
+@route('/static/<filename>')
+def server_static(filename):
+	return static_file(filename, root='./assets/images')
+
 	
 #Pages
 #1.2 Server functionality
@@ -31,5 +36,7 @@ tickets = [
 def index():
 	#need this function to attatch decorators above
 	pass
-	
-run(host='0.0.0.0', port=8080, reloader= True, debug=True)
+
+#reloader = True breaks the code? Only at home PC though???? apparantly is a server issue
+run(host='localhost', port=8080, debug=True)
+#run(host='0.0.0.0', port=8080, reloader= True, debug=True)
